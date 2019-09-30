44eab5f31182fa8c6aec0a715dcec5b27ab2fc6d
wht <wjzhwht@hotmail.com>
2019-09-30 17:42:07 +0800

    MsgBox.h、ExcelOper.h、ParseAdaptNo.h\.cpp更新

--- ToolCode/MsgBox.h
+++ ToolCode/MsgBox.h
@@ -1,34 +1,16 @@
 #pragma once
+#ifndef __MSG_BOX_INC____
+#define __MSG_BOX_INC____
 // 定义一个钩子句柄
-HHOOK g_hMsgBoxHook;
-char BTN_ID_YES_TEXT[100]="是";
-char BTN_ID_NO_TEXT[100]="否";
-char BTN_ID_CANCEL_TEXT[100]="关闭";
-char BTN_ID_OK_TEXT[100]="打开文件夹";
-char BTN_ID_IGNORE_TEXT[100]="忽略";
-char BTN_ID_RETRY_TEXT[100]="重试";
+extern HHOOK g_hMsgBoxHook;
+extern char BTN_ID_YES_TEXT[100];
+extern char BTN_ID_NO_TEXT[100];
+extern char BTN_ID_CANCEL_TEXT[100];
+extern char BTN_ID_OK_TEXT[100];
+extern char BTN_ID_IGNORE_TEXT[100];
+extern char BTN_ID_RETRY_TEXT[100];
 
 // 定义CBT钩子过程
-LRESULT CALLBACK MsgBoxCBTProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
-	switch (nCode) {
-	case HCBT_ACTIVATE:
-		HWND hWnd = (HWND)wParam;
-		SetDlgItemText(hWnd,IDYES,BTN_ID_YES_TEXT);
-		SetDlgItemText(hWnd,IDNO,BTN_ID_NO_TEXT);
-		SetDlgItemText(hWnd,IDCANCEL,BTN_ID_CANCEL_TEXT);
-		SetDlgItemText(hWnd,IDOK,BTN_ID_OK_TEXT);
-		SetDlgItemText(hWnd,IDRETRY,BTN_ID_RETRY_TEXT);
-		SetDlgItemText(hWnd,IDIGNORE,BTN_ID_IGNORE_TEXT);
-		return 0;
-	}
-	return CallNextHookEx(g_hMsgBoxHook, nCode, wParam, lParam);
-}
-
-int MsgBox(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType)
-{
-	g_hMsgBoxHook = SetWindowsHookEx(WH_CBT, MsgBoxCBTProc, NULL, GetCurrentThreadId());
-	int ret = MessageBox(hWnd, lpText, lpCaption, uType);
-	UnhookWindowsHookEx(g_hMsgBoxHook);
-	return ret;
-}
+LRESULT CALLBACK MsgBoxCBTProc(int nCode, WPARAM wParam, LPARAM lParam);
+int MsgBox(HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType);
+#endif
